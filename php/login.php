32ec148f52ae4b4da9fc870cbbbeaff663ce139d
Fabián Lozano Ballesteros <fabianlozano044@gmail.com>
2021-08-04 16:19:31 -0500

    Uni el login que creo Eugenio y arregle bugs de inicio de sesion y creacion de usuario

--- php/login.php
+++ php/login.php
@@ -4,14 +4,28 @@ include 'bd.php';
 $correo= $_POST['correo'];
 
 $pass= $_POST['pass'];
-$validar_login = mysqli_query($conexion, "SELECT * FROM users WHERE correo='$correo' and pass='$pass'");
+$validar_login = mysqli_query($conexion, "SELECT * FROM users WHERE correo='$correo'");
+$row = $validar_login ->fetch_assoc();
 
 if (mysqli_num_rows($validar_login)>0) {
-    $_SESSION['user']=$correo;
-    //require_once("../index.html")
-    header("location: welcome.php");
-    exit;
+    $row = $validar_login ->fetch_assoc();
+    $_pass = $row['pass'];
+    $verify = hash('sha512', $pass);    
+    if(password_verify($verify, $_pass)) {
+        $_SESSION['user']=$correo;
+        header("location: ../index.html");
+        exit;
+    }else{
+	header("location: ../index.php");
+        echo '
+        <script>
+        alert("usuario o contraseña incorrecta, veririfica los datos nuevamente");
+        </script>
+        ';
+        exit;
+        }
 }else{
+    header("location: ../index.php");
     echo '
     <script>
     alert("usuario inexistente, veririfica los datos nuevamente");
