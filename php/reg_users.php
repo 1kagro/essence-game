22b1296f5350c6261805ac7880f2cb717f80e56e
Fabián Lozano Ballesteros <fabianlozano044@gmail.com>
2021-08-09 19:31:42 -0500

    validacion de registro de usuario

--- php/reg_users.php
+++ php/reg_users.php
@@ -1,5 +1,12 @@
 <?php
-include 'bd.php';
+/* include_once 'bd.php';*/
+include_once 'user_session.php';
+include_once 'user.php';
+
+$_user = new User();
+$userSession = new UserSession();
+
+
 
 $name= $_POST['nom'];
 $correo= $_POST['correo'];
@@ -8,12 +15,53 @@ $pass= $_POST['pass'];
 
 $pass = password_hash(hash('sha512', $pass), PASSWORD_DEFAULT);
 
-$query = "INSERT INTO users(nom, correo, user, pass)
- VALUES ('$name','$correo','$user','$pass') ";
+if($_user->correoExist($correo)) {
+    echo '
+    <script>
+        alert("Este correo ya existe");
+        window.location= "../index.php"
+    </script>
+    ';
+    exit();
+}
 
-$verificar_correo = mysqli_query($conexion, "SELECT * FROM users WHERE correo='$correo'");
+if($_user->userOnlyExist($user)) {
+    echo '
+    <script>
+        alert("Este nombre de usuario ya existe");
+        window.location= "../index.php"
+    </script>
+    ';
+    exit();
+}
 
-if(mysqli_num_rows($verificar_correo)>0){
+$query = $_user->setData($name, $correo, $user, $pass);
+
+if($query) {
+    echo '
+    <script>
+        alert("Usuario creado exitosamente");
+        window.location= "../index.php"
+    </script>
+    ';
+}else{
+    echo '
+    <script>
+        alert("Intentelo mas tarde");
+        window.location= "../index.php"
+    </script>
+    ';
+}
+
+
+/* $query = "INSERT INTO users(nom, correo, user, pass)
+ VALUES ('$name','$correo','$user','$pass') "; */
+
+
+/* $verificar_correo = mysqli_query($conexion, "SELECT * FROM users WHERE correo='$correo'"); */
+
+
+/* if(mysqli_num_rows($verificar_correo)>0){
     echo '
     <script>
     alert("Este correo ya existe");
@@ -22,11 +70,11 @@ if(mysqli_num_rows($verificar_correo)>0){
     ';
     exit();
 
-}
+} */
 
-$verificar_user = mysqli_query($conexion, "SELECT * FROM users WHERE user='$user'");
+/* $verificar_user = mysqli_query($conexion, "SELECT * FROM users WHERE user='$user'"); */
 
-if(mysqli_num_rows($verificar_user)>0){
+/* if(mysqli_num_rows($verificar_user)>0){
     echo '
     <script>
     alert("Este nombre de usuario ya existe");
@@ -35,15 +83,15 @@ if(mysqli_num_rows($verificar_user)>0){
     ';
     exit();
 
-}
+} */
 
 
 
 
-$ejecutar= mysqli_query($conexion, $query);
+/* $ejecutar= mysqli_query($conexion, $query); */
 
 
-if ($ejecutar) {
+/* if ($ejecutar) {
     echo '
     <script>
     alert("Usuario creado exitosamente");
@@ -62,5 +110,5 @@ if ($ejecutar) {
     </script>
     ';
 }
-mysqli_close($conexion);
+mysqli_close($conexion); */
 ?>
